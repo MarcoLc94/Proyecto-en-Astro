90e20f1565f2cb88762fd686215357fd16a4c5fd
Marco López <marcoantoniolopezcastro@hotmail.com>
2024-08-20 21:29:38 -0600

    fix

---
+++ .netlify/build/pages/index.astro.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('../chunks/index_BocLu_k1.mjs');
+
+export { page };
---
+++ .netlify/v1/functions/ssr/.netlify/build/pages/thanks.astro.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('../chunks/thanks_DAFW6aUq.mjs');
+
+export { page };
